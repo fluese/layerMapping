cae38af585e03cba9b80a733b4f5b905a661c45c
fluese <36738075+fluese@users.noreply.github.com>
2022-05-23 15:49:33 +0200

    Delete removeBiasfield_sensemap.sh

--- removeBiasfield_sensemap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-data=$1
-
-echo ""
-echo "------------------------------------------------------------------------------"
-echo "| Performing bias field correction using SPM12"
-echo "------------------------------------------------------------------------------"
-matlab -nosplash -nodisplay -r "preproc_sensemap('"${data}"'); exit;"
